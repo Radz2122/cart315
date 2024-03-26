c0c1c38fe657ba4331b153d14e412113449d02ba
Radz2122 <radhika2122@icloud.com>
2024-03-26 14:34:14 -0400

    prototype2-iteration 3
    flipping charcter sprite based on walking direction

--- prototype/Assets/PlayerController.cs
+++ prototype/Assets/PlayerController.cs
@@ -6,12 +6,13 @@ public class PlayerController : MonoBehaviour
     
 
     private Rigidbody2D rb;
+     private SpriteRenderer spriteRenderer;
  
     void Start()
     {
         // Get the Rigidbody2D component
         rb = GetComponent<Rigidbody2D>();
-
+     spriteRenderer = GetComponent<SpriteRenderer>();
      
     }
 
@@ -20,6 +21,15 @@ public class PlayerController : MonoBehaviour
         // Get player input for movement
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+
+        if (horizontalInput > 0) // Moving right
+        {
+            spriteRenderer.flipX = false; // No flip
+        }
+        else if (horizontalInput < 0) // Moving left
+        {
+            spriteRenderer.flipX = true; // Flip horizontally
+        }
 
         // Calculate movement direction
         Vector2 direction = new Vector2(horizontalInput, verticalInput).normalized;
