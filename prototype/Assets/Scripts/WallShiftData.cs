9ab87f92cc6be18b5e2c37118e5e5e84a152a637
Radz2122 <radhika2122@icloud.com>
2024-04-03 23:56:05 -0400

    added script to store data of the walls shifting

---
+++ prototype/Assets/Scripts/WallShiftData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class WallShiftData : MonoBehaviour
+{
+    public float shiftAmountX = 1.0f; // Amount to shift the wall on the X axis
+    public float shiftAmountY = 1.0f; // Amount to shift the wall on the Y axis
+}
